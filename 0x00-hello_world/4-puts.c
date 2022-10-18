af04e5dac1ec2508c3b6d1265e64731790181e0a
mwachi21 <mwachidennis567@gmail.com>
2022-10-18 10:38:13 -0700

    First C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
-*main - print a line of code using puts
+*main -print a line of code using puts
 *
 *Return: 0
 */
@@ -10,5 +9,5 @@ int main(void)
 {
        	puts("\"Programming is like building a multilingual puzzle");
 
-Return(0);
+Return (0);
 }
